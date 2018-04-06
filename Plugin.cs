5b6782f27dc378b5a73d6fedae364392117ee61e
David Pires <david.ferreira.pires@everis.com>
2018-04-06 17:18:34 +0100

    Initial Configuration

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Subble.Core;
+using Subble.Core.Config;
+using Subble.Core.Logger;
+using Subble.Core.Plugin;
+
+namespace SocketCommunication
+{
+    public class Plugin : ISubblePlugin
+    {
+        public IPluginInfo Info
+            => new PluginInfo();
+
+        public SemVersion Version
+            => new SemVersion(0, 0, 1);
+
+        public long LoadPriority => 10;
+
+        public IEnumerable<Dependency> Dependencies
+            => new[]
+            {
+                new Dependency(typeof(ILogger), 0, 0, 1),
+                new Dependency(typeof(IConfigManager), 0, 0, 1)
+            };
+
+
+        public bool Initialize(ISubbleHost host)
+        {
+            if (host is null)
+                return false;
+                
+            return true;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,20 @@
+﻿using Subble.Core.Plugin;
+using System;
+
+namespace SocketCommunication
+{
+    public class PluginInfo : IPluginInfo
+    {
+        public string GUID => "aa20ab12-5dfe-4b37-9c2b-0fa48dcbd1e8";
+
+        public string Name => "SocketCommunication";
+
+        public string Creator => "David Pires";
+
+        public string Repository => "https://github.com/Subble/SocketCommunication";
+
+        public string Support => "https://github.com/Subble/SocketCommunication/issues";
+
+        public string Licence => "MIT";
+    }
+}
