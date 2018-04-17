4e2954c1bcf0efcd44d6da1dd684a0d4e21b6d77
David Pires <david.ferreira.pires@everis.com>
2018-04-17 12:01:21 +0100

    Added tests and change structure to match subble

---
+++ tests/Tests.cs
@@ -0,0 +1,15 @@
+#if DEBUG
+using Xunit;
+
+namespace SocketCommunication.Tests
+{
+    public class SocketTests
+    {
+        [Fact]
+        public void Socket_Tests()
+        {
+            Assert.True(true);
+        }
+    }
+}
+#endif
